e52d62bc4af0e585bae65d9284a347d9453ee4c5
Pedro <pedrofrancach@gmail.com>
2023-01-31 18:13:15 -0300

    commit

---
+++ linear/linear.rb
@@ -0,0 +1,38 @@
+# Algoritimo de busca simples
+def busca(arraye,indice)
+    
+cont = 0.to_i
+tamanho = arraye.length
+ver = true
+
+while cont < tamanho do
+    
+    ver = true
+    
+    if arraye[cont] == indice
+    
+       puts "O índice procurado encontra-se na posição #{cont}"
+
+    else   
+         
+        ver = false 
+
+    end
+
+    cont = cont + 1
+
+end
+
+if ver == false
+
+    puts "O índice não foi encontrado"
+
+end
+end
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+lista = [10,20,30,40,50,60,70,80,90]
+pesquisa = 110
+
+
+busca(lista,pesquisa)
