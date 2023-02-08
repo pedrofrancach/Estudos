543aa1328fcbe75c9fe5595b78ba3fa7d8a60c93
pedro <pedrofrancach@gmail.com>
2023-02-08 20:05:39 -0300

    commit

--- bubble/bubble.rb
+++ bubble/bubble.rb
@@ -2,9 +2,30 @@
 
 def bubble(arraye)
 
+arrayi = []
+cont = 0 
+tamanho = arraye.length
+tamanho = tamanho - 1
 
 
+while cont < tamanho do 
+ 
+    if arraye[cont] < arraye[cont + 1]
 
+        arrayi[cont] = arraye[cont]
+
+    else
+
+        arrayi[cont] = arraye[cont + 1]
+
+    end
+
+    cont = cont + 1
+
+end 
+puts arraye
+puts "##############"
+puts arrayi
 
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
