bfe28913896ff7afc4a2797a5ca3babda6e1c74b
pedro <pedrofrancach@gmail.com>
2023-02-11 12:23:01 -0300

    commit

--- bubble/bubble.rb
+++ bubble/bubble.rb
@@ -3,41 +3,50 @@
 def bubble(arraye)
 
 arrayi = []
-cont = 0.to_i 
+cont1 = 0.to_i
+cont2 = 1.to_i 
 tamanho = arraye.length
-tamanho = 2
+tamanho = tamanho -1
 var = 0
-puts arraye[cont + 1]
-puts "#######"
-while cont < tamanho do 
- puts cont
 
+while cont1 < tamanho do 
+puts "#?#?#?#"
+   puts cont1
+   puts cont2 
+puts "#?#?#?#"
   
-    if arraye[cont] < arraye[cont + 1]
-       puts "é isso aqui1 #{arraye[cont]}" 
-       var = arraye[cont]
+    if arraye[cont1] < arraye[cont2]
+       
+       puts "é isso aqui1 #{arraye[cont1]}" 
+       var = arraye[cont1]
        puts "abcde #{var}"      
-       arrayi[cont] = var
+       arrayi.push(var)
+       #arrayi[cont1] = var
 
     else
-       puts "é isso aqui2 #{arraye[cont + 1]}"
-       var = arraye[cont + 1]
-       puts "mnopq #{var}"
-       arrayi[cont] = var
+
+       puts "é isso aqui2 #{arraye[cont2]}"
+       var = arraye[cont2]
+       #puts "mnopq #{var}"
+       arrayi.push(var)
+       #arrayi[cont1] = var
 
     end
-  
+
        
-    cont = cont + 1
+    cont1 = cont1 + 1
+    cont2 = cont2 + 1
 
 end 
+
 #puts arraye
-puts "##############"
-puts arrayi
+#puts "##############"
+#puts arrayi
 
 end
+
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-lista = [5,1,4,2,8]
+lista = [5,34,4,18,8]
 
 bubble(lista)
