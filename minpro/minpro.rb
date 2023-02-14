b9632ce85e4b6ef370dd3eaf4d3995698ca21d28
pedro <pedrofrancach@gmail.com>
2023-02-14 11:45:14 -0300

    commit

--- minpro/minpro.rb
+++ minpro/minpro.rb
@@ -5,17 +5,18 @@ def minpro(arraye)
 cont = 0.to_i
 tamanho =  arraye.length
 soma = 0.to_i
+tamanho = tamanho -1
 
 while cont <= tamanho do
    
 
     if soma == 0
-        
+        puts "entrou"
         soma = arraye[cont]
         soma = soma * arraye[cont + 1]
     
     else
-    
+    puts "entrou2"
         soma = soma * arraye[cont] 
 
     end
