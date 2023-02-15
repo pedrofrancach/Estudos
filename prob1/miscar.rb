45d9bb22b68683d820eba178abba6cc8ab652db7
pedro <pedrofrancach@gmail.com>
2023-02-15 19:59:52 -0300

    commit

---
+++ prob1/miscar.rb
@@ -0,0 +1,24 @@
+#Missing caracter
+
+def missing(arraye)
+
+tamanho = arraye.length
+cont = 0.to_i 
+Alfabeto = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,z]
+
+
+
+
+
+end
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+
+
+puts "digite a frase para verificação"
+lista = gets
+
+
+
+missing(lista)
