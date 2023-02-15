6e1807c8ff903f6057700425679e8c91928ce46e
pedro <pedrofrancach@gmail.com>
2023-02-15 20:25:22 -0300

    0001

--- prob1/miscar.rb
+++ prob1/miscar.rb
@@ -2,13 +2,28 @@
 
 def missing(arraye)
 
-tamanho = arraye.length
-cont = 0.to_i 
-Alfabeto = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,z]
+tamanho = arraye.length 
+cont1 = 0.to_i 
+cont2 = 0.to_i
+alfabeto = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,z]
+tamanho2 = alfabeto.length
+arrayi = []
 
+while cont1 < tamanho do
 
+    while cont2 < tamanho2 do
+    
+        if not arraye[cont1].include?(alfabeto[cont2]) 
 
+            arrayi << alfabeto[cont2]   
+        
+        end
+            
+        cont2 = cont2 + 1
+    
+    end
 
+cont1 = cont1 + 1
 
 end
 
