99c78f61daf4858d5ca27cdb52c2e7df06540c14
pedro <pedrofrancach@gmail.com>
2023-02-17 07:14:57 -0300

    commit

--- prob2/count.rb
+++ prob2/count.rb
@@ -1,1 +1,17 @@
 # Contador de letras maiúsculas, minúsculas, carcteres especiais e numerais
+
+def count(arraye)
+    
+
+
+
+
+end
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+puts " digite a frase para verificação..."
+
+lista = gets
+
+
+count(lista)
