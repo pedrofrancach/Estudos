bfc0e4abdc59f03f8ab3d6ffbda9180cdbc23c82
pedro <pedrofrancach@gmail.com>
2023-02-17 07:37:40 -0300

    commit

--- prob2/count.rb
+++ prob2/count.rb
@@ -2,6 +2,30 @@
 
 def count(arraye)
     
+tamanho = arraye.length
+cont = 0.to_i
+cont2 = 0.to_i
+numeros = "0123456789"
+guarda_num = 0
+alfabeto = "qwertyuioplkjhgfdsazmxncbv"
+
+while cont < tamanho do
+    
+    while cont2 < 11 do
+
+        if arraye[cont] == numeros[cont2]
+            
+            guarda_num = guarda_num + 1
+
+        end
+
+        cont2 = cont2 + 1
+
+    end 
+
+    cont = cont + 1
+
+end
 
 
 
