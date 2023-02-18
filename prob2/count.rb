26493cbb0d695181f5d4485cbf5c4d11af204031
pedro <pedrofrancach@gmail.com>
2023-02-18 09:25:44 -0300

    commit

--- prob2/count.rb
+++ prob2/count.rb
@@ -7,6 +7,9 @@ cont1 = 0.to_i
 cont2 = 0.to_i
 numeros = "0123456789"
 guarda_num = 0
+guard_let_max = 0
+guard_let_min = 0
+guard_special = 0
 alfabeto_min = "qwertyuioplkjhgfdsazmxncbv"
 alfabeto_max = "QWERTYUIOPASDFGHJKLZXCVBNM"
 
@@ -19,7 +22,13 @@ while cont1 < tamanho do
         
             guarda_num = guarda_num + 1
 
-        end
+        elsif arraye[cont1] == alfabeto_min[cont2]
+
+            guard_let_min = guard_let_min + 1
+        
+        elsif arraye[cont1] == alfabeto_max[cont2]
+
+         
 
         cont2 = cont2 + 1
 
@@ -27,53 +36,7 @@ while cont1 < tamanho do
 
     cont1 = cont1 + 1
     cont2 = 0
-
-end
-
-cont1 = 0
-cont2 = 0
-guard_let_min = 0
-
-while cont1 < tamanho do
-
-    while cont2 < 27
-
-        if arraye[cont1] == alfabeto_min[cont2]
-            
-            guard_let_min = guard_let_min + 1  
-
-        end
     
-            cont2 = cont2 + 1
-
-    end
-
-    cont1 = cont1 + 1
-    cont2 = 0
-
-end
-
-cont1 = 0
-cont2 = 0
-guard_let_max = 0
-
-while cont1 < tamanho do
-
-    while cont2 < 27 do
-    
-        if arraye[cont1] == alfabeto_max[cont2]
-            
-            guard_let_max = guard_let_max + 1
-
-        end
-
-        cont2 = cont2 + 1
-
-    end
-
-    cont1 = cont1 + 1
-    cont2 = 0
-
 end
 
 puts "A quantidade de letras maiúsculas é: #{guard_let_max}"
