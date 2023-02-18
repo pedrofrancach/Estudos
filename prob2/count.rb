d654971ababc03c4d4729c1505467d0de7c65392
pedro <pedrofrancach@gmail.com>
2023-02-18 08:50:36 -0300

    commit

--- prob2/count.rb
+++ prob2/count.rb
@@ -3,19 +3,19 @@
 def count(arraye)
     
 tamanho = arraye.length
-cont = 0.to_i
+cont1 = 0.to_i
 cont2 = 0.to_i
 numeros = "0123456789"
 guarda_num = 0
 alfabeto = "qwertyuioplkjhgfdsazmxncbv"
 
 
-while cont < tamanho do
+while cont1 < tamanho do
     
     while cont2 < 11 do
 
-        if arraye[cont] == numeros[cont2]
-            puts "entrou 2"
+        if arraye[cont1] == numeros[cont2]
+        
             guarda_num = guarda_num + 1
 
         end
@@ -24,13 +24,35 @@ while cont < tamanho do
 
     end 
 
-    cont = cont + 1
+    cont1 = cont1 + 1
     cont2 = 0
 
 end
 
-puts guarda_num
+cont1 = 0
+cont2 = 0
+guard_let_min = 0
 
+while cont1 < tamanho do
+puts "entrou1"
+    while cont2 < 27
+    puts "entrou2"
+        if arraye[cont1] == alfabeto[cont2]
+            
+            guard_let_min = guard_let_min + 1  
+
+        end
+    
+            cont2 = cont2 + 1
+
+    end
+
+    cont1 = cont1 + 1
+    cont2 = 0
+    
+end
+
+puts guard_let_min
 
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
