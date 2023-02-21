410cdabb4690ff3239ad4f49f7373ad8ad09d51c
pedro <pedrofrancach@gmail.com>
2023-02-21 09:40:08 -0300

    commit

--- prob4/reverse.rb
+++ prob4/reverse.rb
@@ -1,9 +1,36 @@
 # Reversal
-def Rev
+def rev(arraye)
+
+
+tamanho = arraye.length - 1
+primeiro = 0
+segundo = 0
+terceiro = 0
+cont = 0
+arm = 0
+
+while cont < tamanho do
+puts "entrou1"    
+    if arraye[cont] > arraye[cont + 1]
+    puts "entrou if"
+        arm = arraye[cont + 1]      
+        arraye[cont + 1] = arraye[cont]
+        arraye[cont] = arm
+    
+    end
+
+cont = cont + 1
+
+end
+
+puts arraye
+
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 
-lista = [1,2,3,4,5,6,7,8,9,10]
+lista = [10,9,8,7,6,5,4,3,2,1]
+#lista = [1,2,3,4,5,6,7,8,9,10]
 
 rev(lista)
+
