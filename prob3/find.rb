2245c5ce85ef31b2bf5a6c030760c70b9c18ab73
pedro <pedrofrancach@gmail.com>
2023-02-21 08:25:44 -0300

    commit

--- prob3/find.rb
+++ prob3/find.rb
@@ -1,10 +1,38 @@
 #Find Theree
 def findtheree(arraye)
 
+tamanho = arraye.length
+cont = 0.to_i
+arm1 = []
+arm2 = 0 
 
 
+while cont < tamanho do
 
+    if arraye[cont] < arraye[cont + 1] && arraye[cont + 1] <= tamanho 
     
+        arm1 << arraye[cont + 1]
+    
+    end
+
+end
+
+
+tamanho = arm1.length
+cont = 0 
+
+
+while cont < tamanho do
+
+    if arm1[cont] < arm1[cont + 1]
+    
+        arm2 << arraye[cont + 1]
+    
+    end 
+
+end 
+   
+    puts arm2
     
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#
