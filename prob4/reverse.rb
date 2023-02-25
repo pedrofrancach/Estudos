feba0781b18ea4a2ccd5449c6bfbc9b471f8d189
pedro <pedrofrancach@gmail.com>
2023-02-25 08:26:18 -0300

    commit

--- prob4/reverse.rb
+++ prob4/reverse.rb
@@ -3,26 +3,44 @@ def rev(arraye)
 
 arrayi = []
 tamanho = arraye.length - 1
+cont = 0
+cont2 = 0
 primeiro = 0
 segundo = 0
 terceiro = 0
-cont = 0
+
 arm = 0
 
 
-while cont < tamanho do
+while cont2 < tamanho do
 
-    if arraye[cont] > arraye[cont + 1]
+    while cont < tamanho do
+
+        if arraye[cont] > arraye[cont + 1]
     
-        arm = arraye[cont + 1]      
-        arraye[cont + 1] = arraye[cont]
-        arraye[cont] = arm
+            arm = arraye[cont + 1]      
+            arraye[cont + 1] = arraye[cont]
+            arraye[cont] = arm
     
-    end
+        end
 
-cont = cont + 1
+        cont = cont + 1
 
-end 
+    end 
+
+    cont = 0
+    cont2 = cont2 + 1
+
+end
+
+puts "###########"
+puts arraye
+puts "##########"
+
+primeiro = arraye[tamanho]
+segundo = arraye[tamanho - 1]
+terceiro = arraye [tamanho - 2]
+
 
 puts arraye
 
@@ -30,7 +48,7 @@ end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 
-lista = [10,9,8,7,6,5,4,3,2,1]
+lista = [10,9,8,45,7,6,5,69,4,18,3,2,1]
 #lista = [1,2,3,4,5,6,7,8,9,10]
 
 rev(lista)
