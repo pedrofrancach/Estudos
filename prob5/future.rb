7c8cc75cb792234154a8253ea4b0396ac1e24746
pedro <pedrofrancach@gmail.com>
2023-03-06 21:30:59 -0300

    commit

--- prob5/future.rb
+++ prob5/future.rb
@@ -72,7 +72,7 @@ while cont1 < tamanho do
     mes = data[1].to_i
     dia = data[0].to_i 
 
-    while cont2 <= consultas do
+    while cont2 < consultas do
     
          data2 = query[cont2].split('/')
          ano2 = data2[2].to_i
@@ -84,7 +84,7 @@ while cont1 < tamanho do
         
         if ano2 < ano
         
-        arrayi << data2   
+        arrayi << query[cont2]  
         
         end
 
