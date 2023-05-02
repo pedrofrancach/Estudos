452b67c51864b0688b215a58e1aafe58a69a8ede
pedro <pedrofrancach@gmail.com>
2023-05-02 10:23:31 -0300

    commit

--- prob6/calc.rb
+++ prob6/calc.rb
@@ -1,5 +1,16 @@
 ##Calculo da Idade
+def calcid(datai, dataa)
 
-
-
+end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+
+puts "Digite o data de nascimento"
+
+datan = gets
+
+puts "Digite a data de calculo"
+
+dataa = gets
+
+def calid(datai, dataa)
