3c4921244ed5966540b0d2fa32b8257461e7f82f
pedro <pedrofrancach@gmail.com>
2023-05-01 21:27:40 -0300

    commit

--- prob3/find.rb
+++ prob3/find.rb
@@ -3,12 +3,15 @@ def findtheree(arraye)
 
 tamanho = arraye.length 
 cont = 0.to_i
-primeiro = 0
-segundo = 0 
-terceiro = 0
+primeiro = 0.to_i
+segundo = 0.to_i 
+terceiro = 0.to_i
+
+#puts arraye
 
 while cont < tamanho do
-
+    #puts "a rodada é #{cont}" 
+    #puts arraye[cont]
     if primeiro < arraye[cont]  
 
         
@@ -34,8 +37,10 @@ end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#
 
 #lista = [10,4,3,50,23,24,90]#*
-
-
+ 
+#puts "Digite os números para serem avaliados"
+#lista = gets
+lista = [5690,312,10589,32567,25814,123,5874,65]
 
 findtheree(lista)
 
