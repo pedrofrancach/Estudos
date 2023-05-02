5de01c4f226e02a0b8d0c81ab709379f9e854e6b
pedro <pedrofrancach@gmail.com>
2023-05-02 10:33:14 -0300

    commit

--- prob6/calc.rb
+++ prob6/calc.rb
@@ -1,6 +1,21 @@
 ##Calculo da Idade
 def calcid(datai, dataa)
 
+puts "As datas são#{datai} e #{dataa}"
+
+
+datainicial = datai.split("/")
+anoi = datainicial[2]
+mesi = datainicial[1]
+diai = datainicial[0]
+
+
+puts "O ano é #{anoi}"
+puts "O mês é #{mesi}"
+puts "O dia é #{diai}"
+
+
+
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
@@ -13,4 +28,4 @@ puts "Digite a data de calculo"
 
 dataa = gets
 
-def calid(datai, dataa)
+calcid(datan, dataa)
